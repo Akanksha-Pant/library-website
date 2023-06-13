91cae5500f490d4499adfd501b835b8a3ea430d2
sonalsk <sonalkushwaha039@gmail.com>
2023-06-13 22:32:58 +0530

    navbar, about, faculty

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ import {DNDBullet, DNDCard} from './components/dndCard.js'
 import {ServiceCard} from './components/serviceCard.js'
 import {RuleCard} from './components/ruleCard.js'
 import {DBCard} from './components/dbCard.js'
+import { FacultyCard } from './components/facultyCard.js';
+import { DetailsCard } from './components/detailsCard.js';
 
 customElements.define('dnd-bullet', DNDBullet);
 customElements.define('dnd-card', DNDCard);
 customElements.define('service-card', ServiceCard);
 customElements.define('rule-card', RuleCard);
 customElements.define('db-card', DBCard)
+customElements.define('faculty-card', FacultyCard);
+customElements.define('details-card', DetailsCard);
